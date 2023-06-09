31061b5634912e015c29252618109bfde5db8541
Setiadi Pratama <idaites23@gmail.com>
2023-06-09 20:17:17 +0700

    Switch Case

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,18 +1,59 @@
 let btn = document.querySelectorAll(".drum");
 
 for (let i = 0; i < btn.length; i++) {
-    btn[i].addEventListener("click", () => {
-        // let audio = new Audio('./sounds/crash.mp3');
-        // audio.play();
-        btn[i].style.color = "white"
-    })
-    btn[i].addEventListener("mouseleave", () => {
-        // let audio = new Audio('./sounds/crash.mp3');
-        // audio.play();
-        btn[i].style.color = "#da0463";
-    })
-}
+  btn[i].addEventListener("click", () => {
+    btn[i].style.color = "white";
 
+    let btnInner = btn[i].innerHTML;
+
+    switch (btnInner) {
+      case "w":
+        let tom1 = new Audio("./sounds/tom-1.mp3");
+        tom1.play();
+        break;
+
+      case "a":
+        let tom2 = new Audio("./sounds/tom-2.mp3");
+        tom2.play();
+        break;
+
+      case "s":
+        let tom3 = new Audio("./sounds/tom-3.mp3");
+        tom3.play();
+        break;
+
+      case "d":
+        let tom4 = new Audio("./sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+      case "j":
+        let snare = new Audio("./sounds/snare.mp3");
+        snare.play();
+        break;
+
+      case "k":
+        let crash = new Audio("./sounds/crash.mp3");
+        crash.play();
+        break;
+
+      case "l":
+        let kick = new Audio("./sounds/kick-bass.mp3");
+        kick.play();
+        break;
+
+      default:
+        console.log("first", btnInner);
+        break;
+    }
+  });
+
+  btn[i].addEventListener("mouseleave", () => {
+    // let audio = new Audio('./sounds/crash.mp3');
+    // audio.play();
+    btn[i].style.color = "#da0463";
+  });
+}
 
 //* CATATAN
 /* function add(num1, num2) {
