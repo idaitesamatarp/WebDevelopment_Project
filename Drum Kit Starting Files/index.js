145f2fd6b0fdf4da9114b5ca42744e72d38b0c0a
Setiadi Pratama <idaites23@gmail.com>
2023-06-23 14:51:59 +0700

    Advanced Javascript and DOM Manipulation

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,58 +1,71 @@
+// Directing button click
+
 let btn = document.querySelectorAll(".drum");
 
 for (let i = 0; i < btn.length; i++) {
   btn[i].addEventListener("click", () => {
-    btn[i].style.color = "white";
-
     let btnInner = btn[i].innerHTML;
-
-    switch (btnInner) {
-      case "w":
-        let tom1 = new Audio("./sounds/tom-1.mp3");
-        tom1.play();
-        break;
-
-      case "a":
-        let tom2 = new Audio("./sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-      case "s":
-        let tom3 = new Audio("./sounds/tom-3.mp3");
-        tom3.play();
-        break;
-
-      case "d":
-        let tom4 = new Audio("./sounds/tom-4.mp3");
-        tom4.play();
-        break;
-
-      case "j":
-        let snare = new Audio("./sounds/snare.mp3");
-        snare.play();
-        break;
-
-      case "k":
-        let crash = new Audio("./sounds/crash.mp3");
-        crash.play();
-        break;
-
-      case "l":
-        let kick = new Audio("./sounds/kick-bass.mp3");
-        kick.play();
-        break;
-
-      default:
-        console.log("first", btnInner);
-        break;
-    }
+    keyboardPress(btnInner);
+    buttonAnimation(btnInner);
   });
+}
 
-  btn[i].addEventListener("mouseleave", () => {
-    // let audio = new Audio('./sounds/crash.mp3');
-    // audio.play();
-    btn[i].style.color = "#da0463";
-  });
+// Direction keyboard press
+document.addEventListener("keypress", function (event) {
+  keyboardPress(event.key);
+  buttonAnimation(event.key);
+});
+
+function keyboardPress(key) {
+  switch (key) {
+    case "w":
+      let tom1 = new Audio("./sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      let tom2 = new Audio("./sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      let tom3 = new Audio("./sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      let tom4 = new Audio("./sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      let snare = new Audio("./sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      let crash = new Audio("./sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      let kick = new Audio("./sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+      console.log("first", btnInner);
+      break;
+  }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(() => {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
 
 //* CATATAN
--- Javascript Object/index.js
+++ Javascript Object/index.js
@@ -10,6 +10,9 @@ function HouseKeeper(yearsOfExperience, name, cleaningExperience) {
   this.yearsOfExperience = yearsOfExperience;
   this.name = name;
   this.cleaningExperience = cleaningExperience;
+  this.clean = function () {
+    alert("Cleaning in progress...");
+  };
 }
 
 var houseKeeper1 = new HouseKeeper(5, "Marie", [
@@ -19,3 +22,29 @@ var houseKeeper1 = new HouseKeeper(5, "Marie", [
 ]);
 
 var houseKeeper2 = new HouseKeeper(2, "Jane", ["bedroom"]);
+
+houseKeeper1.clean();
+
+//* CONSTRUCTOR FUNCTION
+function Audio(fileLocation) {
+  this.fileLocation = fileLocation;
+  this.play = function () {
+    // Tap into the audio hardware
+    // Check the file at fileLocation exists
+    // Check the file at fileLocation is a sound file
+    // Play the file at fileLocation
+  };
+}
+
+//* CALLBACK FUNCTION
+function addAnotherEventListener(typeOfEvent, callback) {
+  var eventThatHappened = {
+    eventType: "keypress",
+    key: "p",
+    durationKeypress: 2,
+  };
+
+  if (eventThatHappened.eventType == typeOfEvent) {
+    callback(eventThatHappened);
+  }
+}
