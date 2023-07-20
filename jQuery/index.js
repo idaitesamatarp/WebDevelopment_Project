56f6cd30ba5c745ab4fa1f5afdc0a30f66c205f3
setiadi <setiadip459@gmail.com>
2023-07-20 09:52:16 +0700

    jquery

--- jQuery/index.js
+++ jQuery/index.js
@@ -4,22 +4,34 @@ $(document).ready(function () {
   //* Manipulating Style
   // $("h1").css("color", "red");
   //* Manipulating Text
-  // $("h1").text("Bye");
-  // $("button").text("<em>Hey</em>");
+  /* $("h1").text("Bye");
+  $("button").text("<em>Hey</em>"); */
   //* Manipulating Attributes
-  // console.log($("img").attr("src"));
-  // $("a").attr("href", "https://www.yahoo.com");
+  /* console.log($("img").attr("src"));
+  $("a").attr("href", "https://www.yahoo.com"); */
   //* Adding Event Listener
-  // $(document).keypress(function (e) {
-  //   $("h1").text(e.key);
-  // });
-  // $(document).on("mouseover", function (e) {
-  //   $("h1").css("color", "purple");
-  // });
+  /* $(document).keypress(function (e) {
+    $("h1").text(e.key);
+  });
+  $(document).on("mouseover", function (e) {
+    $("h1").css("color", "purple");
+  }); */
   //* Adding and Removing Elements
-  // $("h1").before("<button>New</button>");
-  // $("h1").after("<button>New</button>");
-  // $("h1").prepend("<button>New</button>");
-  // $("h1").append("<button>New</button>");
-  // $("button").remove();
+  /* $("h1").before("<button>New</button>");
+  $("h1").after("<button>New</button>");
+  $("h1").prepend("<button>New</button>");
+  $("h1").append("<button>New</button>");
+  $("button").remove(); */
+  //* Animations
+  /* $("button").click(function () {
+  $("h1").hide();
+  $("h1").show();
+  $("h1").toggle();
+  $("h1").fadeOut();
+  $("h1").fadeToggle();
+  $("h1").slideUp();
+  $("h1").slideToggle();
+  $("h1").animate({ opacity: 0.5 });
+  $("h1").slideUp().slideDown().animate({ opacity: 0.5 });
+  }); */
 });
