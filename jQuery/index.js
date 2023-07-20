e9b38e571021af01542f57f9ba84ee0374baa4be
setiadi <setiadip459@gmail.com>
2023-07-20 09:36:50 +0700

    jquery

---
+++ jQuery/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  //* Selecting Element
+  // $("h1").css("color");
+  //* Manipulating Style
+  // $("h1").css("color", "red");
+  //* Manipulating Text
+  // $("h1").text("Bye");
+  // $("button").text("<em>Hey</em>");
+  //* Manipulating Attributes
+  // console.log($("img").attr("src"));
+  // $("a").attr("href", "https://www.yahoo.com");
+  //* Adding Event Listener
+  // $(document).keypress(function (e) {
+  //   $("h1").text(e.key);
+  // });
+  // $(document).on("mouseover", function (e) {
+  //   $("h1").css("color", "purple");
+  // });
+  //* Adding and Removing Elements
+});
